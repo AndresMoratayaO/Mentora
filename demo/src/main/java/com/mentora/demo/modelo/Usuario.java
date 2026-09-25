252046ec115b9fee5eec5763e2786441dc52bb6e
AndresMoratayaO <andrese.moratayao@gmail.com>
2026-09-25 01:15:24 -0600

    Base de datos conectada

---
+++ demo/src/main/java/com/mentora/demo/modelo/Usuario.java
@@ -0,0 +1,59 @@
+package com.mentora.demo.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity 
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(nullable = false, unique = true, length = 254)
+    private String correo;
+
+    @Column(name = "contrasena_hash", nullable = false, length = 255)
+    private String contrasenaHash;
+
+    @Column(name = "tipo_usuario", nullable = false, length = 20)
+    private String tipoUsuario;
+
+    protected Usuario(){
+
+    }
+
+    public Usuario(String nombre, String correo, String contrasenaHash, String tipoUsuario){
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasenaHash = contrasenaHash;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getContrasenaHash() {
+        return contrasenaHash;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+}
